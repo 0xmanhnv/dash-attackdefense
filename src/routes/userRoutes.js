acfcbdb0dbaf7fb6a4e0f920d2cd8d4c0978848a
manhnv <nguyenmanh0397@gmail.com>
2024-11-29 14:56:41 +0700

    Add roles

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -3,8 +3,14 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
 const authMiddleware = require('../middlewares/authMiddleware');
+const roleMiddleware = require('../middlewares/roleMiddleware');
 
 // Lấy thông tin người dùng (yêu cầu xác thực)
 router.get('/profile', authMiddleware.verifyToken, userController.getProfile);
+
+// Route chỉ dành cho admin
+router.get('/admin', authMiddleware.verifyToken, roleMiddleware.checkRole(['admin']), (req, res) => {
+    res.send('Admin content');
+});
 
 module.exports = router;
